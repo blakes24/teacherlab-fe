24650bd5c850e6a604a85e9e7f6dace9fce93a8f
dgriego <eggomygriego@gmail.com>
2021-03-27 18:21:22 -0700

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx}', './components/**/*.{js,ts,jsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
