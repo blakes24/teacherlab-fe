96e9a5866ad7632758cba1d930d318d7765c3540
Daniel Griego <eggomygriego@gmail.com>
2021-04-02 15:20:48 -0700

    add protectedFetcher

--- services/utils.js
+++ services/utils.js
@@ -1,9 +1,39 @@
 import { API_BASE_URL } from "./config";
+import { isAuthenticated } from "./auth";
+
+const CONTENT_TYPE_JSON = {
+  "Content-Type": "application/json",
+};
 
 const fetcher = (...args) => {
-  return fetch(`${API_BASE_URL.local}${args[0]}`, args[1]).then((res) =>
+  const options = {
+    ...args[1],
+    headers: {
+      ...CONTENT_TYPE_JSON,
+      ...args[1].headers,
+    },
+  };
+
+  return fetch(`${API_BASE_URL.local}${args[0]}`, options).then((res) =>
     res.json()
   );
 };
 
-export { fetcher };
+const protectedFetcher = (...args) => {
+  const token = isAuthenticated();
+
+  const options = {
+    ...args[1],
+    headers: {
+      ...CONTENT_TYPE_JSON,
+      ...args[1].headers,
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  return fetch(`${API_BASE_URL.local}${args[0]}`, options).then((res) =>
+    res.json()
+  );
+};
+
+export { fetcher, protectedFetcher };
