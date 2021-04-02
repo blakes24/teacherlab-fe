9d233bee272947e51759124b9229ab47b90707c1
Daniel Griego <eggomygriego@gmail.com>
2021-04-02 14:33:43 -0700

    add UnitCardForm properties for input values

--- components/unit/unit-card-form.js
+++ components/unit/unit-card-form.js
@@ -1,19 +1,59 @@
 import Input from "../common/input";
 import Button from "../common/button";
+import { UNIT_FORM_DATA_SYMBOLS } from "../../libs/constants";
 import PropTypes from "prop-types";
 
-export default function UnitCardForm({ closeAction }) {
+export default function UnitCardForm({
+  unitNumber,
+  unitTitle,
+  startDate,
+  endDate,
+  reviewDate,
+  onChange,
+  closeAction,
+}) {
+
   function handleUnitCreation(e) {
     e.preventDefault();
   }
 
   return (
     <form onSubmit={handleUnitCreation} className="grid grid-cols-1 gap-3">
-      <Input type="number" label="unit number" required></Input>
-      <Input type="text" label="unit title" required></Input>
-      <Input type="date" label="start date" required></Input>
-      <Input type="date" label="end date" required></Input>
-      <Input type="date" label="review date" required></Input>
+      <Input
+        onChange={onChange.bind(null, UNIT_FORM_DATA_SYMBOLS.unitNumber)}
+        value={unitNumber}
+        type="number"
+        label="unit number"
+        required
+      ></Input>
+      <Input
+        onChange={onChange.bind(null, UNIT_FORM_DATA_SYMBOLS.unitTitle)}
+        value={unitTitle}
+        type="text"
+        label="unit title"
+        required
+      ></Input>
+      <Input
+        onChange={onChange.bind(null, UNIT_FORM_DATA_SYMBOLS.startDate)}
+        value={startDate}
+        type="date"
+        label="start date"
+        required
+      ></Input>
+      <Input
+        onChange={onChange.bind(null, UNIT_FORM_DATA_SYMBOLS.endDate)}
+        value={endDate}
+        type="date"
+        label="end date"
+        required
+      ></Input>
+      <Input
+        onChange={onChange.bind(null, UNIT_FORM_DATA_SYMBOLS.reviewDate)}
+        value={reviewDate}
+        type="date"
+        label="review date"
+        required
+      ></Input>
       <div className="flex justify-around">
         <Button
           text="Cancel"
@@ -37,4 +77,9 @@ export default function UnitCardForm({ closeAction }) {
 
 UnitCardForm.propTypes = {
   closeAction: PropTypes.func,
+  unitNumber: PropTypes.string,
+  unitTitle: PropTypes.string,
+  startDate: PropTypes.object,
+  endDate: PropTypes.object,
+  reviewDate: PropTypes.object,
 };
