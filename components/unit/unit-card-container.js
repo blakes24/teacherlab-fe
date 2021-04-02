91a245cb99c86535de3e63cf3d7d10bf515c88dd
Daniel Griego <eggomygriego@gmail.com>
2021-04-02 14:34:11 -0700

    update UnitCardContainer to handle form data updates

--- components/unit/unit-card-container.js
+++ components/unit/unit-card-container.js
@@ -1,12 +1,44 @@
-import Input from "../common/input";
-import Button from "../common/button";
 import UnitCardForm from "./unit-card-form";
 import PropTypes from "prop-types";
+import { UNIT_FORM_DATA_SYMBOLS } from "../../libs/constants";
+import { useState } from "react";
 
 export default function UnitCardContainer({ closeAction }) {
+  const [unitNumber, setUnitNumber] = useState("1");
+  const [unitTitle, setUnitTitle] = useState("");
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [reviewDate, setReviewDate] = useState();
+
+  function handleUnitFormDataChange(inputSymbol, event) {
+    const value = event.target.value;
+
+    switch (inputSymbol) {
+      case UNIT_FORM_DATA_SYMBOLS.unitNumber:
+        setUnitNumber(value);
+        break;
+      case UNIT_FORM_DATA_SYMBOLS.unitTitle:
+        setUnitTitle(value);
+        break;
+      case UNIT_FORM_DATA_SYMBOLS.startDate:
+        setStartDate(value);
+        break;
+      case UNIT_FORM_DATA_SYMBOLS.endDate:
+        setEndDate(value);
+        break;
+      case UNIT_FORM_DATA_SYMBOLS.reviewDate:
+        setReviewDate(value);
+        break;
+    }
+  }
+
   return (
     <div className="bg-blue w-72 mx-auto p-7  pt-4">
-      <UnitCardForm closeAction={closeAction}></UnitCardForm>
+      <UnitCardForm
+        onChange={handleUnitFormDataChange}
+        closeAction={closeAction}
+        unitNumber={unitNumber}
+      ></UnitCardForm>
     </div>
   );
 }
