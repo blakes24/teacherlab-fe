e97a54893991c81198c1209477334e66ca9256b5
Daniel Griego <eggomygriego@gmail.com>
2021-04-04 20:30:52 -0700

    add getUnit fetch

--- services/unit.js
+++ services/unit.js
@@ -1,8 +1,14 @@
 import { protectedFetcher } from "./utils";
 
 export const createUnit = function (data) {
-  return protectedFetcher(`/units`, {
+  return protectedFetcher("/units", {
     method: "POST",
     body: JSON.stringify(data),
   });
 };
+
+export const getUnit = function(unitId) {
+  return protectedFetcher(`/units/${unitId}`, {
+    method: "GET"
+  });
+}
