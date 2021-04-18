33811cd03bb2046df4f4b7bb625a4ce642186d0c
Daniel Griego <eggomygriego@gmail.com>
2021-04-17 19:49:30 -0700

    Adding SideBar and adjusting layout
    * defines and organizes layout specific components to separate logic for each
    * threw in a fix in the even that details was null when setting state for a unit

---
+++ components/layout/nav.js
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <nav className="flex justify-between h-14 px-6 items-center bg-blue text-white uppercase text-xl tracking-wider relative z-10">
+      <div className="flex-shrink ml-auto">Teacher Lab</div>
+    </nav>
+  );
+}
