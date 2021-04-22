a0a8cecbe948a4e7328298f2d38c5e341dce817d
blakes24 <blakes24@gmail.com>
2021-04-22 16:08:33 -0700

    Add ability to save collaboration answers

--- components/unit/question-input.js
+++ components/unit/question-input.js
@@ -1,16 +1,28 @@
-export default function QuestionInput({ id, text, value }) {
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { addAnswer } from "../../store/unit-slicer";
+
+export default function QuestionInput({ id, label, value }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="bg-gray-100 p-3 w-full">
       <label className="ml-1" htmlFor={id}>
-        {text}
+        {label}
       </label>
       <textarea
         id={id}
         className="w-full h-11 rounded-md mt-2"
         value={value}
-        onChange={null}
+        onChange={(event) => dispatch(addAnswer({  id, value:  event.target.value  }))}
         placeholder="Type Here..."
       ></textarea>
     </div>
   );
 }
+
+QuestionInput.propTypes = {
+  id: PropTypes.number,
+  label: PropTypes.string,
+  value: PropTypes.string,
+};
